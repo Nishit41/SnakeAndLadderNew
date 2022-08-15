9e1233bb35481d3caf042487b88e66a15b1b9234
Nishit <nishitranjan41@gmail.com>
2022-08-15 22:54:44 +0530

    This is my SnakeAndLadderProblem

---
+++ src/com/bridgelabz/basics/SnakeAndLadder.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.basics;
+
+public class SnakeAndLadder {
+    static int currentPosition = 0 ;
+    public static void main(String[] args) {
+        System.out.println("Welcome To Snake and Ladder");
+        System.out.println("StartPosition =>"+currentPosition);
+    }
+}
