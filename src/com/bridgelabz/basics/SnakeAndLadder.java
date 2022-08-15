2f0b975249ac58a2f21ca7cdb645e09433be84e1
Nishit <nishitranjan41@gmail.com>
2022-08-15 22:57:43 +0530

    this is welcome message

--- src/com/bridgelabz/basics/SnakeAndLadder.java
+++ src/com/bridgelabz/basics/SnakeAndLadder.java
@@ -4,6 +4,5 @@ public class SnakeAndLadder {
     static int currentPosition = 0 ;
     public static void main(String[] args) {
         System.out.println("Welcome To Snake and Ladder");
-        System.out.println("StartPosition =>"+currentPosition);
     }
 }
