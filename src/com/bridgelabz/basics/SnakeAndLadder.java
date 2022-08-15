6d34b536646d02cff2b48f8c577002701cce37db
Nishit <nishitranjan41@gmail.com>
2022-08-15 23:05:01 +0530

    this is my STARTING position

--- src/com/bridgelabz/basics/SnakeAndLadder.java
+++ src/com/bridgelabz/basics/SnakeAndLadder.java
@@ -4,5 +4,6 @@ public class SnakeAndLadder {
     static int currentPosition = 0 ;
     public static void main(String[] args) {
         System.out.println("Welcome To Snake and Ladder");
+        System.out.println("Starting Position =>"+" "+currentPosition);
     }
 }
