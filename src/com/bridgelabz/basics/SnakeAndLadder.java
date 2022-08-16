7b8a0dedacad81b3a128c8ceb57b4ea4ffc2194b
Nishit <nishitranjan41@gmail.com>
2022-08-16 15:09:26 +0530

    this is  about generaterandom

--- src/com/bridgelabz/basics/SnakeAndLadder.java
+++ src/com/bridgelabz/basics/SnakeAndLadder.java
@@ -1,9 +1,12 @@
 package com.bridgelabz.basics;
 
 public class SnakeAndLadder {
-    static int currentPosition = 0 ;
+    static int currentPosition = 0;
+
     public static void main(String[] args) {
         System.out.println("Welcome To Snake and Ladder");
-        System.out.println("Starting Position =>"+" "+currentPosition);
+        System.out.println("Starting Position =>" + " " + currentPosition);
+            int dice =( int )(Math.random() * 10)%6+1;
+            System.out.println(dice);
     }
 }
