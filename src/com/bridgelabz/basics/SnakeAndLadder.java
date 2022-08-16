d3a902d1fad038da3a572fcf39e6882c7c2d5bc4
Nishit <nishitranjan41@gmail.com>
2022-08-16 16:11:35 +0530

    this is  about getoption,SnakeLAdder,Noplay

--- src/com/bridgelabz/basics/SnakeAndLadder.java
+++ src/com/bridgelabz/basics/SnakeAndLadder.java
@@ -1,12 +1,32 @@
 package com.bridgelabz.basics;
 
-public class SnakeAndLadder {
-    static int currentPosition = 0;
-
+    public class SnakeAndLadder {
+        static int position = 0;
+        static final int SNAKE = 2;
+        static final int NO_PLAY = 1;
+        static final int LADDER = 3;
+        static int dieRoll(){
+        return (int)(Math.random()*10)%6+1;
+    }
+    static int getOption(){
+        return (int)(Math.random()*10)%3+1;
+    }
     public static void main(String[] args) {
         System.out.println("Welcome To Snake and Ladder");
-        System.out.println("Starting Position =>" + " " + currentPosition);
-            int dice =( int )(Math.random() * 10)%6+1;
-            System.out.println(dice);
+        System.out.println("Starting Position =>" + " " + position);
+         int dieNum = dieRoll();
+         int option = getOption();
+         switch (option){
+             case SNAKE :
+                 position -= dieNum;
+                 break;
+             case LADDER :
+                 position += dieNum;
+                 break;
+             case NO_PLAY:
+                 break;
+             default:
+         }
+
     }
 }
